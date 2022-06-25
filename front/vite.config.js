3a074d593bfb8701a9cf1b87dfdb71f27f96186f
Gowther <AcevedoAriel@outlook.com>
2022-06-25 19:32:32 -0300

    Conflicto arreglado

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,6 +4,10 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
   plugins: [
     react(),
     VitePWA({
