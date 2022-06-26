d943f55af12ad3a62f143b952b1cb4310f6afd86
Gaston Acosta <gastoneacosta@outlook.com>
2022-06-26 04:06:39 -0300

    Inicializado la pantalla principal
    solo nombre, fondo y configuracion inicial

---
+++ front/src/components/BarNavigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BarNavigation = () => {
+  return (
+    <div>
+      <p class='text-center text-8xl font-extrabold pb-2 pt-1 text-white tracking-wider font-monstserrat'>
+
+        FANTUR
+      </p>
+    </div>
+  )
+}
+
+export default BarNavigation
