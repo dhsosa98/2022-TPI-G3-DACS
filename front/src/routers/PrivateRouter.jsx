62142b2a1447db9fd4fa2d858c46656a48e658d1
Diego <dhsosa98@gmail.com>
2022-07-05 20:59:47 -0300

    fix issue

--- front/src/routers/PrivateRouter.jsx
+++ front/src/routers/PrivateRouter.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Redirect, Route } from "react-router-dom";
-import { useState } from 'react';
 
 const PrivateRouter = ({ component: Component, ...resto }) => {
   const [auth, setAuth] = useState(false);
--- front/src/routers/PublicRouter.jsx
+++ front/src/routers/PublicRouter.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Redirect, Route } from "react-router-dom";
-import { useState } from 'react';
 
 const PublicRouter = ({ component: Component, ...resto }) => {
   const [auth, setAuth] = useState(false);
