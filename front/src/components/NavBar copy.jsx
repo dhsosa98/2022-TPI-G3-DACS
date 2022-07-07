db24c14ee34e1aefe996333b543d80595f2a1ce2
Gaston Acosta <gastoneacosta@outlook.com>
2022-07-06 22:18:15 -0300

    agregado modal cerrar sesion

--- front/src/components/NavBar copy.jsx
+++ front/src/components/NavBar copy.jsx
@@ -15,8 +15,6 @@ import {
 import { ChevronDownIcon } from "@heroicons/react/solid";
 import { useContext } from "react";
 import { AuthContext } from "../contexts/Auth";
-import ModalCerrarSesion from "./ModalCerrarSesion";
-import { useState } from "react";
 
 const resources = [
   {
@@ -88,18 +86,13 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-
-
 export default function NavBar() {
-  const [isOpen, setIsOpen] = useState(false)
-  
   const { auth, logout } = useContext(AuthContext);
   return (
     <Popover className="relative bg-[#ffffffcc]">
       {({ open, close }) => (
         <>
           <div className="max-w-7xl mx-auto px-4 sm:px-6">
-            {isOpen && <ModalCerrarSesion open={isOpen} setOpen={setIsOpen}/>}
             <div className="flex justify-between items-center border-gray-100 py-2 md:justify-start md:space-x-10">
               <div className="flex justify-start lg:w-0 lg:flex-1">
                 <Link to="/">
@@ -199,6 +192,71 @@ export default function NavBar() {
                   )}
                 </Popover>
 
+                {/* INFO PARA EL ADMIN             */}
+                <Popover className="relative">
+                  {({ open, close }) => (
+                    <>
+                      <Popover.Button
+                        className={classNames(
+                          open ? "text-gray-900" : "text-gray-500",
+                          "group rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 "
+                        )}
+                      >
+                        <span>Formularios</span>
+                        <ChevronDownIcon
+                          className={classNames(
+                            open ? "text-gray-600" : "text-gray-400",
+                            "ml-2 h-5 w-5 group-hover:text-gray-500"
+                          )}
+                          aria-hidden="true"
+                        />
+                      </Popover.Button>
+
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-500"
+                        enterFrom="opacity-0 translate-y-1"
+                        enterTo="opacity-100 translate-y-0"
+                        leave="transition ease-in duration-150"
+                        leaveFrom="opacity-100 translate-y-0"
+                        leaveTo="opacity-0 translate-y-1"
+                      >
+                        <Popover.Panel className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-md sm:px-0">
+                          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                            <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                              {formsAdmin.map((item) => (
+                                <NavLink
+                                  onClick={() => close()}
+                                  activeStyle={{
+                                    backgroundColor: "#0ed4d4f9",
+                                    textDecoration: "none",
+                                  }}
+                                  key={item.name}
+                                  to={item.href}
+                                  className="-m-3 p-3 flex items-start rounded-lg hover:bg-[#00ffff41]"
+                                >
+                                  <item.icon
+                                    className="flex-shrink-0 h-6 w-6 text-indigo-600 "
+                                    aria-hidden="true"
+                                  />
+                                  <div className="ml-4 ">
+                                    <p className="text-base font-medium text-gray-900">
+                                      {item.name}
+                                    </p>
+                                    <p className="mt-1 text-sm text-gray-500">
+                                      {item.description}
+                                    </p>
+                                  </div>
+                                </NavLink>
+                              ))}
+                            </div>
+                          </div>
+                        </Popover.Panel>
+                      </Transition>
+                    </>
+                  )}
+                </Popover>
+                
               </Popover.Group>
               <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
                 {!auth ? (
@@ -220,18 +278,13 @@ export default function NavBar() {
                 >
                   Registrarse
                 </NavLink></>) : (
-                  <button onClick={()=>{
-                     setIsOpen(true)
-                  }} 
-                    
-                    
-                    className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-[#00adad] hover:bg-[#00adad86]">
+                  <button onClick={()=>{logout()}} className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-[#00adad] hover:bg-[#00adad86]">
                     Cerrar Sesion
                   </button>)}
               </div>
             </div>
           </div>
-         
+
           <Transition
             as={Fragment}
             enter="duration-200 ease-out"
