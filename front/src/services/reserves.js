54f8ee8523456d387c816c658d6bc1b272811a00
Gowther <AcevedoAriel@outlook.com>
2022-07-08 00:36:03 -0300

    Reserva de paquete desde usuario

--- front/src/services/reserves.js
+++ front/src/services/reserves.js
@@ -1,17 +1,25 @@
-import Axios from '../config/Axios';
-import { API_BASE_URL } from '../vite-env.d';
+import Axios from "../config/Axios";
+import { API_BASE_URL } from "../vite-env.d";
 
 export const getBookings = async () => {
-    const response = await Axios.get(API_BASE_URL +"/reserves?limit=100");
-    return response.data;
-}
+  const response = await Axios.get(API_BASE_URL + "/reserves?limit=100");
+  return response.data;
+};
 
 export const getBooking = async (id) => {
-    const response = await Axios.get(`${API_BASE_URL}/reserves/${id}`);
-    return response.data;
-}
+  const response = await Axios.get(`${API_BASE_URL}/reserves/${id}`);
+  return response.data;
+};
 
 export const deleteBooking = async (id) => {
-    const response = await Axios.delete(API_BASE_URL+"/reserves/"+id);
-    return response.data;
-}
+  const response = await Axios.delete(API_BASE_URL + "/reserves/" + id);
+  return response.data;
+};
+
+export const createBooking = async (uid, id) => {
+  const response = await Axios.post(API_BASE_URL + "/reserves", {
+    userId: uid,
+    reserve: { packagesByClient: id },
+  });
+  return response.data;
+};
