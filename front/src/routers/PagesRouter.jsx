c0ed07e9e8d98a85d654b3fbdb8aa1a27ba43dc6
TomasVilalta <tomasfedericovilalta@gmail.com>
2022-07-08 00:33:54 -0300

    Added user profile page

--- front/src/routers/PagesRouter.jsx
+++ front/src/routers/PagesRouter.jsx
@@ -22,6 +22,7 @@ import Registrarse from "../pages/Registrarse";
 import PublicRouter from "./PublicRouter";
 import { Payment } from "../components/Payment";
 import Reservas from "../pages/Reservas";
+import Perfil from "../pages/Perfil";
 
 const PagesRouter = () => {
   return (
@@ -44,6 +45,7 @@ const PagesRouter = () => {
         <Route path="/seguros" component={Seguros} />
         <Route path="/compra-paquete" component={Payment} />
         <Route path="/mis-reservas" component={Reservas} />
+        <Route path="/mis-datos" component={Perfil} />
 
       </Switch>
     </>
